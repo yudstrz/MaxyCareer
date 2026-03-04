1f41d3d1452171eb3462a02e8a37c418125755a3
yud! <wahyudi.contacts@gmail.com>
2026-03-04 14:09:11 +0700

    hapus file yang tidak digunakan

--- kitalulus_test.php
+++
@@ -1,4 +0,0 @@
-<?php
-$html = file_get_contents('https://www.kalibrr.id/job-board/te/developer/1', false, stream_context_create(['http' => ['header' => 'User-Agent: Mozilla/5.0']]));
-file_put_contents('kalibrr_out.html', $html);
-echo "Saved Kalibrr HTML (" . strlen($html) . " bytes)\n";
